d9087b0d008e2010f00f37b0c2d96ec091f8d85e
Amit Maurya <139799731+amit-maurya0099@users.noreply.github.com>
2024-12-30 19:10:02 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/Marknox-Assignment/",
+  base:"/Marknox-Assignment",
   plugins: [react()],
 })
